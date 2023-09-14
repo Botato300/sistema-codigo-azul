7b9086e1a8e233aca14ca7db1b786a1af87485ca
Ian <kikatopro@gmail.com>
2023-09-13 22:44:05 -0300

    Main: se agrega pagina inicial

---
+++ js/main.js
@@ -0,0 +1,25 @@
+TSComponents.Card.render('reservas-card-med', {
+       serieId: '174.1_RRVAS_IDOS_0_0_36:percent_change',
+       explicitSign: true,
+       color: '#F9A822',
+       hasChart: 'small',
+       links: "none",
+       title: "Reservas Internacionales"
+   })
+   
+   TSComponents.Card.render('tn-card-med', {
+       serieId: 'tn_arg',
+       hasChart: 'small',
+       links: "none",
+       title: "Tasa de Natalidad de Argentina",
+       hasFrame: false
+   })
+   
+   TSComponents.Card.render('faafem-card-med', {
+       serieId: 'defensa_FAA_0006',
+       color: '#EC407A',
+       hasChart: 'small',
+       links: "none",
+       title: "Personal femenino de la Fuerza Aérea",
+       hasColorBar: false
+   })
