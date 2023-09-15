a4364cd9a194e2b65ef1f8213bea63721b4ece62
Bottari <tomasbottari300@gmail.com>
2023-09-14 21:37:55 -0300

    bugfix

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,19 +1,20 @@
 <?php
-$usuarioBD = "root" ;
-$passBD = "" ;
-$ipBD = "localhost" ;
-$nombreBD = "hospital" ;
+$usuarioBD = "root";
+$passBD = "";
+$ipBD = "localhost";
+$nombreBD = "hospital";
 
-public function conBDOB()
-    {
-        $oConBD = new mysqli($ipBD, $usuarioBD, $passBD, $nombreBD);
-        if ($oConBD->connect_error) {
-            echo "Error al conectar a la base de datos: " . $oConBD->connect_error . "\n";
-            return false;
-        }
-        echo "Conexión exitosa..." . "\n";
-        return true;
+function conBDOB()
+{
+    global $usuarioBD, $passBD, $ipBD, $nombreBD;
+
+    $oConBD = new mysqli($ipBD, $usuarioBD, $passBD, $nombreBD);
+    if ($oConBD->connect_error) {
+        echo "Error al conectar a la base de datos: " . $oConBD->connect_error . "\n";
+        return false;
     }
-?>
+    echo "Conexión exitosa..." . "\n";
+    return true;
+}
 
 conBDOB();
