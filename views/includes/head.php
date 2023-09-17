5e86a384ee0a927aaf067b3982011b39b6e9f2fb
Bottari <tomasbottari300@gmail.com>
2023-09-16 21:40:04 -0300

    head.php: pequeña modificacion de texto

--- views/includes/head.php
+++ views/includes/head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="author" content="Equipo Tecnico de la escuela EESTN N°2">
+<meta name="author" content="Equipo Técnico de la escuela EESTN N°2">
 
 <link href="assets/css/poncho/bootstrap.min.css" rel="stylesheet">
 <link href="assets/css/poncho/fonts.min.css" rel="stylesheet">
