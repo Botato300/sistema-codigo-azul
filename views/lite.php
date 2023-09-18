bd22287d4bce19c14c8889d1bfec1dad60eb7f23
Bottari <tomasbottari300@gmail.com>
2023-09-18 04:11:28 -0300

    pagina para app movil agregada

---
+++ views/lite.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php require_once("includes/head.php"); ?>
+
+    <script defer src="assets/js/lite.js"></script>
+
+    <title>Ver llamados de paciente</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-top navbar-default bg-celeste-argentina">
+            <div class="container">
+                <div>
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="index.php">
+                            <img alt="Logo de la pagina web" src="assets/images/logo.webp" height="50">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="main__container">
+        <div class="main__content">
+            <h1>Pacientes</h1>
+            <ul class="listPatients__container">
+                <li>Paciente 1</li>
+                <li>Paciente 2</li>
+                <li>Paciente 3</li>
+                <li>Paciente 4</li>
+                <li>Paciente 5</li>
+                <li>Paciente 6</li>
+                <li>Paciente 7</li>
+                <li>Paciente 8</li>
+                <li>Paciente 9</li>
+                <li>Paciente 10</li>
+            </ul>
+        </div>
+    </main>
+
+    <?php require_once("includes/footer.php"); ?>
+</body>
+
+</html>
