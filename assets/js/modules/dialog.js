d35212f520ad1ee0967a07a5ad6b7b79ad6156cc
Bottari <tomasbottari300@gmail.com>
2023-09-19 17:35:43 -0300

    dialogs.js: pequeña modificacion

--- assets/js/modules/dialog.js
+++ assets/js/modules/dialog.js
@@ -1,15 +1,15 @@
 export class Dialog {
-    constructor(element) {
-        this.element = element;
-    }
+	constructor(element) {
+		this.element = element;
+	}
 
-    open() {
-        this.element.showModal();
-        document.getElementsByTagName("body")[0].style.overflow = "hidden";
-    }
+	open() {
+		this.element.showModal();
+		document.getElementsByTagName("body")[0].style.overflow = "hidden";
+	}
 
-    close() {
-        this.element.close();
-        document.getElementsByTagName("body")[0].style.overflow = "auto";
-    }
+	close() {
+		this.element.close();
+		document.getElementsByTagName("body")[0].style.overflow = "auto";
+	}
 }
