6cfefc767d3f8d1b2ce0db2ec1bcc4a7c63679f6
Bottari <tomasbottari300@gmail.com>
2023-09-25 22:57:24 -0300

    se corrigio algunas querys

--- assets/js/verifySession.js
+++ assets/js/verifySession.js
@@ -3,13 +3,12 @@ import { getCookie, verifyCookie } from "./modules/cookies.js";
 const TOKEN = getCookie("token");
 
 (async function () {
-    const status = await verifyCookie(TOKEN);
-
     if (TOKEN === undefined) {
         location.href = "/sistema-codigo-azul/ingreso";
         return false;
     }
 
+    const status = await verifyCookie(TOKEN);
     if (!status) {
         location.href = "/sistema-codigo-azul/ingreso";
         return false;
--- models/patientModel.php
+++ models/patientModel.php
@@ -21,51 +21,51 @@ class patientModel
         $stmt = $this->db->prepare("INSERT INTO personas (idPersona, tipoDocumento) VALUES (?, ?)");
         $stmt->bind_param('is', $idPersona, $tipoDocumento);
         $stmt->execute();
-    }    
+    }
 
     public function insertPatient(int $idPersona, string $historiaClinica): void
     {
         $stmt = $this->db->prepare("INSERT INTO pacientes VALUES (?, ?)");
         $stmt->bind_param('is', $idPersona, $historiaClinica);
         $stmt->execute();
-    }    
+    }
 
     public function update(int $idPersona, int $historiaClinica): void
     {
         $stmt = $this->db->prepare("UPDATE personas
                                                         INNER JOIN pacientes
-                                                        ON personas.idPersona = pacientes.historiaClinica
-                                                        SET personas.idPersona = ?
+                                                        ON personas.idRol = pacientes.historiaClinica
+                                                        SET personas.idRol = ?
                                                         WHERE pacientes.historiaClinica = ?");
         $stmt->bind_param('ii', $idPersona, $historiaClinica);
         $stmt->execute();
 
         $stmt = $this->db->prepare("SELECT * FROM personas 
                                                         INNER JOIN pacientes 
-                                                        ON personas.idPersona = pacientes.historiaClinica");
+                                                        ON personas.idRol = pacientes.historiaClinica");
         $stmt->execute();
 
         $result = $stmt->get_result();
 
-        while($row = $result->fetch_assoc())
-            echo "IdPersona: ".$row['idPersona']." Historia Clinica: ".$row['historiaClinica']."<br>";
-    }    
+        while ($row = $result->fetch_assoc())
+            echo "IdPersona: " . $row['idPersona'] . " Historia Clinica: " . $row['historiaClinica'] . "<br>";
+    }
 
     public function selectAll(): void
     {
         $stmt = $this->db->prepare("SELECT * FROM personas 
                                                         INNER JOIN pacientes 
-                                                        ON personas.idPersona = pacientes.historiaClinica");
+                                                        ON personas.idRol = pacientes.historiaClinica");
         $stmt->execute();
 
         $result = $stmt->get_result();
 
-        while($row = $result->fetch_assoc())
-            echo "Idpersona: ".$row['idPersona']."<br>";
-            echo "historiaClinica: ".$row['historiaClinica']."<br>";
-            echo "nombre: ".$row['nombre']."<br>";
-            echo "apellido: ".$row['apellido']."<br>";
-            echo "telefono: ".$row['telefono']."<br>";
+        while ($row = $result->fetch_assoc())
+            echo "Idpersona: " . $row['idPersona'] . "<br>";
+        echo "historiaClinica: " . $row['historiaClinica'] . "<br>";
+        echo "nombre: " . $row['nombre'] . "<br>";
+        echo "apellido: " . $row['apellido'] . "<br>";
+        echo "telefono: " . $row['telefono'] . "<br>";
     }
 
     public function delete(int $historiaClinica): void
@@ -73,27 +73,24 @@ class patientModel
         $stmt = $this->db->prepare("DELETE personas, pacientes
                                                         FROM personas
                                                         INNER JOIN pacientes 
-                                                        ON personas.idPersona = pacientes.historiaClinica
+                                                        ON personas.idRol = pacientes.historiaClinica
                                                         WHERE pacientes.historiaClinica = ?");
         $stmt->bind_param('i', $historiaClinica);
         $stmt->execute();
 
         $stmt = $this->db->prepare("SELECT * FROM personas 
                                                         INNER JOIN pacientes 
-                                                        ON personas.idPersona = pacientes.historiaClinica");
+                                                        ON personas.idRol = pacientes.historiaClinica");
         $stmt->execute();
 
         $result = $stmt->get_result();
 
-        while($row = $result->fetch_assoc())
-        {
-            echo "Idpersona: ".$row['idPersona']."<br>";
-            echo "historiaClinica: ".$row['historiaClinica']."<br>";
-            echo "nombre: ".$row['nombre']."<br>";
-            echo "apellido: ".$row['apellido']."<br>";
-            echo "telefono: ".$row['telefono']."<br>";    
+        while ($row = $result->fetch_assoc()) {
+            echo "Idpersona: " . $row['idPersona'] . "<br>";
+            echo "historiaClinica: " . $row['historiaClinica'] . "<br>";
+            echo "nombre: " . $row['nombre'] . "<br>";
+            echo "apellido: " . $row['apellido'] . "<br>";
+            echo "telefono: " . $row['telefono'] . "<br>";
         }
-}    
+    }
 }
-
-?>
