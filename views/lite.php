24824354947ce9c132525327b05ffe2e46d621d2
Ian <kikatopro@gmail.com>
2023-09-26 16:52:53 -0300

    test

--- views/lite.php
+++ views/lite.php
@@ -1,48 +1,49 @@
 <!DOCTYPE html>
 <html lang="es">
+hola
 
 <head>
-    <?php require_once("includes/head.php"); ?>
+   <?php require_once("includes/head.php"); ?>
 
-    <script defer src="assets/js/lite.js"></script>
+   <script defer src="assets/js/lite.js"></script>
 
-    <title>Ver llamados de paciente</title>
+   <title>Ver llamados de paciente</title>
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-top navbar-default bg-celeste-argentina">
-            <div class="container">
-                <div>
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="index.php">
-                            <img alt="Logo de la pagina web" src="assets/images/logo.webp" height="50">
-                        </a>
-                    </div>
-                </div>
+   <header>
+      <nav class="navbar navbar-top navbar-default bg-celeste-argentina">
+         <div class="container">
+            <div>
+               <div class="navbar-header">
+                  <a class="navbar-brand" href="index.php">
+                     <img alt="Logo de la pagina web" src="assets/images/logo.webp" height="50">
+                  </a>
+               </div>
             </div>
-        </nav>
-    </header>
+         </div>
+      </nav>
+   </header>
 
-    <main class="main__container">
-        <div class="main__content">
-            <h1>Pacientes</h1>
-            <ul class="listPatients__container">
-                <li>Paciente 1</li>
-                <li>Paciente 2</li>
-                <li>Paciente 3</li>
-                <li>Paciente 4</li>
-                <li>Paciente 5</li>
-                <li>Paciente 6</li>
-                <li>Paciente 7</li>
-                <li>Paciente 8</li>
-                <li>Paciente 9</li>
-                <li>Paciente 10</li>
-            </ul>
-        </div>
-    </main>
+   <main class="main__container">
+      <div class="main__content">
+         <h1>Pacientes</h1>
+         <ul class="listPatients__container">
+            <li>Paciente 1</li>
+            <li>Paciente 2</li>
+            <li>Paciente 3</li>
+            <li>Paciente 4</li>
+            <li>Paciente 5</li>
+            <li>Paciente 6</li>
+            <li>Paciente 7</li>
+            <li>Paciente 8</li>
+            <li>Paciente 9</li>
+            <li>Paciente 10</li>
+         </ul>
+      </div>
+   </main>
 
-    <?php require_once("includes/footer.php"); ?>
+   <?php require_once("includes/footer.php"); ?>
 </body>
 
 </html>
