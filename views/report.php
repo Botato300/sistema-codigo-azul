79902fc30140dc000c8e8db1150a4a10bdfb5ae8
Ian <kikatopro@gmail.com>
2023-09-27 15:15:17 -0300

    Se modifican los reportes

--- views/report.php
+++ views/report.php
@@ -4,6 +4,7 @@ require('../libs/fpdf/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',9);
-$pdf->Cell(40,10,'');
+$pdf->Cell(40,10,'2023-09-26,08:00 AM,10:30 AM ,Maria Gonzalez, 45, Apendice laparoscopica, Dr. Juan Perez, Ninguna');
+$pdf->Cell(40,20,'2023-09-26,11:15 AM,12:45 PM, Carlos Rodriguez, 60 , Cirugia cardiaca, Dr. Ana Garcia ,Infeccion postoperatoria');
 $pdf->Output();
 ?>
